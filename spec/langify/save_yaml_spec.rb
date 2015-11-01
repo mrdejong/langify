e934be2cdaecc288b9daadc9e5ae75222e44b007
Alexander de Jong <mrdejong89@gmail.com>
2015-11-01 12:49:28 +0100

    Add more tests to test different scenarious we can expect

--- spec/langify/save_yaml_spec.rb
+++ spec/langify/save_yaml_spec.rb
@@ -3,6 +3,8 @@ require 'langify/save_yaml'
 
 describe Langify::SaveYaml do
   let(:yml) { Langify::SaveYaml.new({lang: 'nl', file: 'profile', path: 'profile'}, "Profiel") }
+  let(:ymlm) { Langify::SaveYaml.new({lang: 'nl', file: 'profile', path: 'profile.username'}, "Gebruikersnaam") }
+  let(:ymla) { Langify::SaveYaml.new({lang: 'nl', file: 'profile', path: 'profile.last_name'}, "Achternaam", { nl: { profile: { username: "Gebruikersnaam" } } } ) }
 
   before(:all) do
     FileUtils::mkdir_p("#{Dir.pwd}/tmp/config/locale")
@@ -23,5 +25,20 @@ describe Langify::SaveYaml do
 
   it 'should be able to generate a valid yaml hash' do
     expect(yml.to_hash).to eq({nl: {profile: "Profiel"}})
+  end
+
+  it 'should allow for multiple keys' do
+    expect(ymlm.to_hash).to eq({nl: {profile: {username: "Gebruikersnaam"}}})
+  end
+
+  it 'should be smart enough to respect existing hashes' do
+    expect(ymla.to_hash).to eq({
+      nl: {
+        profile: {
+          username: "Gebruikersnaam",
+          last_name: "Achternaam"
+        }
+      }
+    })
   end
 end
