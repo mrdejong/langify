2a0b88d3697114d38d0c77f04c9d32e642da8075
Alexander de Jong <mrdejong89@gmail.com>
2015-11-01 12:50:10 +0100

    Abillity to confirt a dotted eliminated string to convert to an hash

--- lib/langify/save_yaml.rb
+++ lib/langify/save_yaml.rb
@@ -1,10 +1,16 @@
+require 'pry'
+
 class Langify::SaveYaml
-  def initialize(path_hash, str)
+  def initialize(path_hash, str, existing_hash = {})
     @locale_path = "#{Dir.pwd}/tmp/config/locale"
     @file_name = "#{path_hash[:lang]}_#{path_hash[:file]}.yml"
     @lang = path_hash[:lang].to_sym
-    @lang_key = path_hash[:path].to_sym
+    @lang_key = path_hash[:path]
     @str = str
+    if existing_hash.empty?
+      existing_hash[@lang] = {}
+    end
+    @existing = existing_hash
   end
 
   def get_file
@@ -16,9 +22,27 @@ class Langify::SaveYaml
   end
 
   def to_hash
-    hash = { }
-    hash[@lang] = { }
-    hash[@lang][@lang_key] = @str
+    hash = @existing
+    keys = Langify::PathString.to_key_array(@lang_key)
+    hash[@lang] = insert_new_key(keys, hash[@lang], @str)
     hash
   end
+
+  private
+    def insert_new_key(keys, hash, value)
+      if keys.any?
+        key = keys.first
+        keys.delete(key)
+
+        if keys.length > 0
+          if hash[key.to_sym].nil?
+            hash[key.to_sym] = { }
+          end
+          insert_new_key(keys, hash[key.to_sym], value)
+        else
+          hash[key.to_sym] = value
+        end
+      end
+      hash
+    end
 end
